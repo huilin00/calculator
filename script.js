9abd1ec2227d6d973c4b1013b047cfcf6ba6a334
huilin00 <huilin.liang@mail.mcgill.ca>
2025-09-18 23:45:20 -0700

    revised calculator logic part 2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let operand1;
 let operand2;
 let operator;
+let regex = /[-+x÷=\.]/;
 
 // define basic operations
 let addition = (operand1, operand2) => operand1 + operand2;
@@ -14,13 +15,12 @@ operate = (operator, operand1, operand2) => {
         case "+": return addition(operand1, operand2);
         case "-": return subtraction(operand1, operand2);
         case "x": return multiplication(operand1, operand2);
-        case "/": return division(operand1, operand2);
+        case "÷": return division(operand1, operand2);
     }
 }
 
 // initialize the display to be 0
 const display = document.querySelector(".display");
-display.textContent = 0;
 const clearButton = document.querySelector("#clearButton");
 clearButton.addEventListener("click", function(){
     display.textContent = "";
@@ -34,13 +34,31 @@ backspaceButton.addEventListener("click", function() {
 
 const buttons = document.querySelectorAll(".cal");
 buttons.forEach((button) => button.addEventListener("click", function() {
-    // if last 
-    let exp = display.textContent + button.textContent; 
-    if (exp[-1] === "." && button.textContent == ".") {
-        // do nothing
-    }
+    // refuse user input if it's NOT a digit and same as the previous enter value
+    // i.e., multiple click of "." would only result in one "."
+    
+    
 
 }));
+
+
+// *** Helper functions ***
+
+// check whether the given input is digit
+const isDigit = (exp) => !regex.test(exp);
+
+// evalute the given expression
+function evaluateExpression(exp) {
+    // numerics -> display it
+    if (!regex.test(exp)) {
+        display.textContent = exp;
+    }
+    // consider now we might need to evaluate the expression if there's 2 operands and 1 operator
+    else {
+
+    }
+    
+}
 
 // evaluation the expression once "=" button is clicked
 const evaluateButton = document.querySelector("#equal");
